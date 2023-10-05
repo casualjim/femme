103fe1f39633b6a2c81c72391f4d25d878432c81
Ivan Porto Carrero <ivan@flanders.co.nz>
2023-10-05 10:01:29 -0700

    actually bump the deps

--- examples/million.rs
+++ examples/million.rs
@@ -2,7 +2,7 @@ use kv_log_macro as log;
 use std::time::Instant;
 
 fn main() {
-    femme::with_level(femme::LevelFilter::Trace);
+    cj_femme::with_level(cj_femme::LevelFilter::Trace);
 
     let start = Instant::now();
     for n in 0..1_000_000 {
